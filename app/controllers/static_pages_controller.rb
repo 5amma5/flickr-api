d8fb681d1143f0794f0df7f564c08e96eba7978b
5amma5 <32971882+5amma5@users.noreply.github.com>
2022-12-28 19:17:39 -0500

    my solution

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,21 @@
 class StaticPagesController < ApplicationController
   def index
+    begin
+      flickr = Flickr.new
+      unless search_params[:user_id].blank?
+        @photos = flickr.photos.search(user_id: search_params[:user_id], extras: :url_q)
+      else
+        @photos = flickr.photos.getRecent(extras: :url_q)
+      end
+    rescue StandardError => e
+      flash[:alert] = "#{e.class}: #{e.message}."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def search_params
+    params.permit(:user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
+  resources :static_pages, only: [:index]
+
+  root 'static_pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
